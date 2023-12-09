8bed7a9efd83cfaa2cfd4ea8de26179f81724166
Navneet Sharma <navneet.sharma@optevus.com>
2023-12-10 00:01:05 +0530

    add MegaBlog project

---
+++ 12MegaBlog/src/appwrite/auth.js
@@ -0,0 +1,63 @@
+import { Client, Account, ID } from 'appwrite'
+import conf from '../conf/conf.js'
+
+export class AuthService { 
+    client = new Client()
+    account;
+    
+    constructor() {
+        this.client
+            .setEndpoint(conf.appwriteUrl)
+            .setProject(conf.appwriteProjectId);
+        this.account = new Account(this.client);
+    }
+
+    async createAccount({ email, password, name }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({ email, password })
+            } else {
+                throw new Error('Failed to create account')
+            }
+            
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async login({ email, password }) {
+        // eslint-disable-next-line no-useless-catch
+        try {
+           return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+
+    async getCurrentUser() {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser :: error :: ", error);
+        }
+
+        return null;
+    }
+
+    async logout() {
+        // eslint-disable-next-line no-useless-catch
+        try {
+            return await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout :: error :: ", error);
+        }
+    }
+  
+}
+
+const authService = new AuthService()
+
+export default authService
---
+++ 12MegaBlog/src/conf/conf.js
@@ -0,0 +1,9 @@
+const conf = {
+    appwriteUrl: String(import.meta.env.VITE_APP_APPWRITE_URL),
+    appwriteProjectId: String(import.meta.env.VITE_APP_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId: String(import.meta.env.VITE_APP_APPWRITE_DATABASE_ID),
+    appwriteCollectionId: String(import.meta.env.VITE_APP_APPWRITE_COLLECTION_ID),
+    appwriteBucketId: String(import.meta.env.VITE_APP_APPWRITE_BUCKET_ID),
+}
+
+export default conf
